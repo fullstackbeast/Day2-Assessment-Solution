ff1e9296bcbf323d115340ab7ab7cf1f66f849ca
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-04-27 05:40:34 +0100

    Complete Solution

--- EncodeString/Program.cs
+++ EncodeString/Program.cs
@@ -8,18 +8,20 @@ namespace EncodeString
         {
             Console.Write("Input any word or sentence of even length: ");
 
-            string inputWord = Console.ReadLine();
+            string inputWord = Console.ReadLine(); // Collect User's Input
 
-            if (inputWord.Length == 0 || inputWord.Length % 2 != 0)
+            if (inputWord.Length == 0 || inputWord.Length % 2 != 0) //checking to make sure the user input is not empty and of even length
             {
                 Console.WriteLine("The word is not of even length");
             }
             else
             {
-                string firstPart = inputWord.Substring(0, inputWord.Length / 2);
-                string secondPart = inputWord.Substring(inputWord.Length / 2);
+                string firstPart = inputWord.Substring(0, inputWord.Length / 2); // getting the first half of the string
+                string secondPart = inputWord.Substring(inputWord.Length / 2); // getting the second half of the string
 
-                Console.WriteLine(Encode(firstPart, secondPart));
+                string encoded = Encode(firstPart, secondPart); //passing both half of the string to the Encode method to get it encoded
+
+                Console.WriteLine(encoded); //displaying the encoded string to the user
             }
         }
 
@@ -29,13 +31,15 @@ namespace EncodeString
             char[] encondedFirstPart = firstPart.ToCharArray();
             char[] encodedSecondPart = secondPart.ToCharArray();
 
-            for (int i = 1; i < firstPart.Length; i += 2)
+            //converting the string to an array of character. Replacing the characters in the string directly will cause a chain reaction getting the character replaced everywhere in the string
+
+            for (int i = 1; i < firstPart.Length; i += 2) //looping through the odd index only
             { 
-                encodedSecondPart[i - 1] = firstPart[i];
-                encondedFirstPart[i] = secondPart[i - 1];
+                encodedSecondPart[i - 1] = firstPart[i]; // replacing the even part of the second half of the string with the odd part of the first half
+                encondedFirstPart[i] = secondPart[i - 1]; // replacing the odd part of the first half of the string with the even part of the second half
             }
 
-            return $"{new string(encondedFirstPart)}{new string(encodedSecondPart)}";
+            return $"{new string(encondedFirstPart)}{new string(encodedSecondPart)}"; //return a concatenation of the encoded halves
         }
     }
 }
--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -7,14 +7,14 @@ namespace Lottery
         static void Main(string[] args)
         {
 
-            Random random = new Random();
-            int randomDigit = random.Next(100, 1000);
+            Random random = new Random(); //creating an instance of the random class
+            int randomDigit = random.Next(100, 1000); //generating a random 3 digits
 
 
-            Console.Write("Enter three number: ");
-            string userInput = Console.ReadLine();
+            Console.Write("Enter three numbers: ");
+            string userInput = Console.ReadLine(); // getting user input
 
-            if (userInput.Length != 3)
+            if (userInput.Length != 3) //validating to make sure user entered 3 numbers 
             {
                 Console.WriteLine("Enter three digits");
             }
@@ -22,17 +22,17 @@ namespace Lottery
             {
                 int userDigit = 0;
 
-                bool isNumbers = int.TryParse(userInput, out userDigit);
+                bool isNumbers = int.TryParse(userInput, out userDigit); //parsing user input into string and validating to make sure its a number only.  
 
-                if (!isNumbers)
+                if (!isNumbers) // checking if it is a valid number
                 {
                     Console.WriteLine("Enter Numbers only");
                 }
                 else
                 {
-                    if (userDigit == randomDigit)
+                    if (userDigit == randomDigit) //checking if the user digit is the same as the gnerated random digits
                     {
-                        Console.WriteLine("You guessed the digits correctly and you have won $10,000!!!");
+                        Console.WriteLine("You guessed the digits correctly and you have won $10,000!!!"); //displaying the message to the user if the number is guessed correctly
                     }
                     else
                     {
@@ -42,13 +42,14 @@ namespace Lottery
 
                         int correctDigit = 0;
 
-                        for (var i = 0; i < 3; i++)
+                        for (var i = 0; i < 3; i++) // looping through the three digits
                         {
-                            if (randomDigitString.IndexOf(userDigitString[i]) != -1) correctDigit++;
+                            if (randomDigitString.IndexOf(userDigitString[i]) != -1) correctDigit++; //getting the count of correct digits
                         }
 
-                        if (correctDigit > 0) Console.WriteLine($"You got {correctDigit} numbers correctly. You have won ${correctDigit * 1000}!!!");
-                        else Console.WriteLine("You guessed wrong!!!");
+                        if (correctDigit > 0) Console.WriteLine($"You got {correctDigit} numbers correctly. You have won ${correctDigit * 1000}!!!"); // displaying the user prize based on the number of correct digits: $1000 per correct digit
+                        
+                        else Console.WriteLine("You guessed wrong!!!"); // displaying the message to the user if none of the digit is correct
 
                     }
                 }
