c2470e302a01bafaa6be8a1ff5349b84b97dd881
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-04-27 06:09:52 +0100

    Complete Solution

--- RectangleArea/Area.cs
+++ RectangleArea/Area.cs
@@ -1,20 +1,23 @@
 namespace RectangleArea
 {
-    public class Area
+    public class Area //Creating a new class: Area
     {
 
         private double Length;
         private double Breadth;
+        //private fields Lengths and Breadth
 
-        public Area(double length, double breadth)
+        public Area(double length, double breadth) //the constructor of the class
         {
             Length = length;
             Breadth = breadth;
+            //setting the value of the fields through then constructor arguments
         }
 
-        public double ReturnArea()
+        public double ReturnArea() // declaring a public method in the class
         {
             return Length * Breadth;
+            //returns the area by returning the product of the length and breadth
         }
     }
 }
--- RectangleArea/Program.cs
+++ RectangleArea/Program.cs
@@ -8,13 +8,18 @@ namespace RectangleArea
         {
             Console.Write("Enter the length of the rectangle: ");
             double length = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Enter the breadth of the rectangle: ");
             double breadth = Convert.ToDouble(Console.ReadLine());
+            //Collecting the length and breadth of the rectangle from the user
 
-            Area area = new Area(length, breadth);
-            double rectangleArea = area.ReturnArea();
+
+            Area area = new Area(length, breadth); // Creating an instance of the area class and passing the entered inputs through the constructor 
+
+            double rectangleArea = area.ReturnArea(); //invoking the ReturnArea method of the area class to get the area of the reactangle for this instance of the class
 
             Console.WriteLine($"The area of the rectangle is: {rectangleArea}");
+            //Displaying the result to the user
 
         }
     }
