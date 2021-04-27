e2c9fbe9b17098af7cdf1b7832e08b7dba47ac8e
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-04-27 05:49:18 +0100

    Complete Solution

--- OddIndex/Program.cs
+++ OddIndex/Program.cs
@@ -11,23 +11,27 @@ namespace OddIndex
             int sum = 0;
 
             // Method 1
-            for (int i = 0; i < numberArray.Length; i++)
+            for (int i = 0; i < numberArray.Length; i++) // looping through the index of the array 1 by 1
             {
-                if (i % 2 != 0)
+                if (i % 2 != 0) //check if the current index is odd
                 {
-                    sum += numberArray[i];
+                    sum += numberArray[i]; //adding the item at the current odd index to sum
                 }
             }
 
             // method 2
-            int index = 1;
-            while (index < numberArray.Length)
+            int index = 1; // using this as the loop initial 
+
+            while (index < numberArray.Length) // checking if the index varable is still less than the length of the array
             {
-                sum += numberArray[index];
-                index+=2;
+                sum += numberArray[index];//adding the item at the current index to sum
+                
+                index+=2; //increasing the index variable by 2 to make sure only odd values are produced
             }
 
-            Console.WriteLine($"The sum of the element of the array at odd index is:  {sum}");
+            Console.WriteLine($"The sum of the element of the array at odd index is:  {sum}"); 
+            //displaying the output to the user 
+            
         }
     }
 }
