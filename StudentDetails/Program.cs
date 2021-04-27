65d4cf1dac2f64f0b54a4ee5f66e1c54190f9c99
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-04-27 06:23:47 +0100

    Complete Solution

--- StudentDetails/Program.cs
+++ StudentDetails/Program.cs
@@ -8,16 +8,23 @@ namespace StudentDetails
         {
             Student student1 = new Student("John", 2, "08021345689", "Abeokuta Ogun State");
             Student student2 = new Student("Sam", 1,"07021345768", "Lekki, Lagos State");
+            // created different instance of the student class and passsed different values through the constructor
+            
 
             Console.WriteLine($"Name of the first student: {student1.Name}");
             Console.WriteLine($"Roll Number of the first student: {student1.RollNumber}");
             Console.WriteLine($"Phone Number of the first student: {student1.PhoneNumber}");
             Console.WriteLine($"Address of the first student: {student1.Address}");
-            Console.WriteLine();
+
+            // displaying the first student details to the user
+            
+            Console.WriteLine(); // just putting a space in between 
+            
             Console.WriteLine($"Name of the second student: {student2.Name}");
             Console.WriteLine($"Roll Number of the second student: {student2.RollNumber}");
             Console.WriteLine($"Phone Number of the second student: {student2.PhoneNumber}");
             Console.WriteLine($"Address of the second student: {student2.Address}");
+            // displaying the second student details to user
         }
     }
 }
--- StudentDetails/Student.cs
+++ StudentDetails/Student.cs
@@ -4,16 +4,20 @@ namespace StudentDetails
     {
         public string Name;
         public int RollNumber;
-
         public string PhoneNumber;
         public string Address;
 
+        // public fields created for the student class
+
         public Student(string name, int rollNumber, string phoneNumber, string address)
+        // creating a constructor for the class as collecting valeues for the fields as argument
         {
             Name = name;
             RollNumber = rollNumber;
             PhoneNumber = phoneNumber;
             Address = address;
+
+            //setting the fields value through the constructor arguments
         }
 
 
